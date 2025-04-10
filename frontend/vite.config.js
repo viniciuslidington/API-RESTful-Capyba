57a4b97adeb0559241dffa0744e42b3209a3a6f8
Vinicius Lidington <vlidington@gmail.com>
2025-04-10 11:32:10 -0300

    conf: viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    PORTA: 3000,
     proxy: {
-      '/api': `http://localhost:${PORTA}`,
+      '/api': `http://localhost:3000`,
     },
 },
 })
