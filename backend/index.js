19ffe1bf3c48eb64cf6d7cfc96d1269767916c53
Vinicius Lidington <vlidington@gmail.com>
2025-04-11 08:21:24 -0300

    task: alt arquitetura e auth

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Rotas
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import upload from "../config/multer.js";
+import { getMe } from "../controllers/authController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", upload.single("image"), registerUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+router.get("/me", verifyToken, getMe);
+
+export default router;
