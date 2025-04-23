341b9bb812abed8305d9022a964d6f2cbb40dab6
Vinicius Lidington <vlidington@gmail.com>
2025-04-23 08:39:07 -0300

    railway

--- backend/index.js
+++ backend/index.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando`);
 });
