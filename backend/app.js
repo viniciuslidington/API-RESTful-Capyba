9e3d8e201eeec75c917927e12ac0fdf1438fb412
Vinicius Lidington <vlidington@gmail.com>
2025-04-23 12:15:02 -0300

    mudanca cors

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://capybacademy.up.railway.app/",
     credentials: true,
 }));
 
