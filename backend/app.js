53aae145e0b02f1391cba4fc751b58f588b5c9c8
Vinicius Lidington <vlidington@gmail.com>
2025-04-23 08:09:42 -0300

    railway config

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:80", "http://localhost:5173"],
+    origin: process.env.FRONTEND_URL,
     credentials: true,
 }));
 
